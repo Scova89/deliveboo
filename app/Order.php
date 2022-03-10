581b84862e46ddcfcdf366adf1f882e9854889ee
AlessandroMandaglio30 <ale.manda.30@gmail.com>
2022-03-10 17:26:07 +0100

    model order

--- app/Order.php
+++ app/Order.php
@@ -4,12 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Order extends Model
 {
+    public function products() {
+        return $this->belongsToMany("App\Product"); 
+    }
     public function user() {
         return $this->belongsTo("App\User"); 
-    }
-    public function orders() {
-        return $this->belongsToMany("App\Order"); 
     }
 }
--- app/User.php
+++ app/User.php
@@ -41,4 +41,7 @@ class User extends Authenticatable
     public function products() {
         return $this->hasMany("App\Product"); 
     }
+    public function orders() {
+        return $this->hasMany("App\Order"); 
+    }
 }
