cda5b9dfa7832d7cd2bc6d22aabca0098408d227
Francesco <scovascova@gmail.com>
2022-03-10 15:23:11 +0100

    migration table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('city', 100);
+            $table->string('slug', 130)->unique();
+            $table->bigInteger('phone')->unique();
+            $table->string('address', 200);
+            $table->boolean('open')->default(true);
+            $table->bigInteger('iva')->unique();
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
