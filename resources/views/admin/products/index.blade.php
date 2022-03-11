3d0e8409ed8a59cc0f1a1c8f2122d4c163f93e83
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-03-11 16:45:38 +0100

    product index

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Lista Prodotti</div>
 
+                    <div class="card-body">
+
+                        <ul>
+                            @foreach ($products as $product)
+                                <li>
+                                    {{$product->name}};
+                                </li>
+                            @endforeach
+                        </ul>
+
+
+
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'Admin\HomeController@index')->name('home');
+Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(
+    function () {   
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('products', 'ProductsController');
+    
+});
