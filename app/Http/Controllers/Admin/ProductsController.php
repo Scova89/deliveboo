716bbbd7ad97b483083b9f0a228def218f4c59d8
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-03-11 17:08:18 +0100

    products create

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -26,7 +26,7 @@ class ProductsController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.products.create');
     }
 
     /**
@@ -37,7 +37,7 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
