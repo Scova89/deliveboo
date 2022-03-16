3feed900055014f163b435ed2906c53f50627d5b
Alessandro <luisialessandro95@gmail.com>
2022-03-16 13:07:02 +0100

    init-layout-backend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,68 +21,39 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    DeliveBoo
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <header>
+            <div class="box">
+                <div class="left">
+                    <a href="{{url('/')}}">
+                        <div class="logo">
+                            <img src="https://www.ilcannolo.com/wp-content/uploads/2020/09/food-ico_Tavola-disegno-1.png" alt="DeliveBoo Logo">
+                        </div>
+                        <div class="title">DeliveBoo</div>
+                    </a>
+                    <nav>
+                        <ul>
                         @if(Auth::user() != null)
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('products.index') }}">Prodotti</a>
-                        </li>
+                            <li >
+                                <a href="{{ route('products.index') }}">Prodotti</a>
+                            </li>
                         @endif
                         @if(Auth::user() != null && Auth::user()->admin)
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('typologies.index') }}">Tipologie</a>
-                        </li>
+                            <li >
+                                <a href="{{ route('typologies.index') }}">Tipologie</a>
+                            </li>
                         @endif
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Accedi</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Registrati</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                    document.getElementById('logout-form').submit();">
-                                        Esci
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="right">
+                    <a href="/login">Area riservata</a>
                 </div>
             </div>
-        </nav>
-
+        </header>
         <main class="py-4">
-            @yield('content')
+            <div class="container-fluid">
+                @yield('content')
+            </div>
         </main>
     </div>
 </body>
