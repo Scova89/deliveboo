477dddb497cde2461fb13cf10381997b4a4184b4
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-03-16 10:48:28 +0100

    verifica email

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">Verifica il tuo indirizzo e-mail</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            Un nuovo link di verifica è stato inviato al tuo indirizzo e-mail.
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Prima di procedere, controlla la tua e-mail per un link di verifica.
+                    Se non hai ricevuto l'e-mail,
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">clicca qui per richiederne un'altra</button>.
                     </form>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Auth;
 //     return view('welcome');
 // });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::prefix("admin")->namespace("Admin")->middleware("auth")->group(
+Route::prefix("admin")->namespace("Admin")->middleware("verified")->group(
     function () {   
     Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('products', 'ProductsController');
