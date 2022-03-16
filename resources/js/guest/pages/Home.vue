1a5c6a05036fb25cb2407cad05bf8ab579b267d2
AlessandroMandaglio30 <ale.manda.30@gmail.com>
2022-03-16 17:13:55 +0100

    animazione card per filtrare la categoria

--- resources/js/guest/pages/Home.vue
+++ resources/js/guest/pages/Home.vue
@@ -1,51 +1,54 @@
 <template>
-    <div>
-        <h4>Scegli una categoria:</h4>
-        <div class="box-card">
-            <Card v-for="categoria in dataShared.categorie" :key="categoria.id" :categoria="categoria"/>
-            
-        </div>
+  <div>
+    <h4>Scegli una categoria:</h4>
+    <div class="box-card">
+      <Card
+        v-for="categoria in dataShared.categorie"
+        :key="categoria.id"
+        :categoria="categoria"
+      />
     </div>
-    
+  </div>
 </template>
 
 <script>
-import Card from './elements/Card.vue';
-import dataShared from '../dataShared';
-
+import Card from "./elements/Card.vue";
+import dataShared from "../dataShared";
 
 export default {
-    name: 'Home',
-    components: {
-        Card
-    },
-    data() {
-        return{
-            dataShared
-        }
-    },
-    created(){
-        axios.get('/api/categorie')
-            .then((response) => {
-                dataShared.categorie = response.data;
-            })
-            .catch( (error) => {
-                this.$router.push({name: 'page-404'})
-            })
-    },
-
-}
+  name: "Home",
+  components: {
+    Card,
+  },
+  data() {
+    return {
+      dataShared,
+    };
+  },
+  created() {
+    axios
+      .get("/api/categorie")
+      .then((response) => {
+        dataShared.categorie = response.data;
+      })
+      .catch((error) => {
+        this.$router.push({ name: "page-404" });
+      });
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    .box-card{
-        display: flex;
-        flex-wrap: wrap;
-        width: 100%;
-        gap: 10px;
-        > * {
-            width: calc((100% - 40px )/ 5);
-        }
-    }
-    
+.box-card {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 10px;
+  > * {
+    width: calc((100% - 40px) / 5);
+  }
+}
+h4 {
+  margin-bottom: 10px;
+}
 </style>
--- resources/js/guest/pages/elements/Card.vue
+++ resources/js/guest/pages/elements/Card.vue
@@ -1,27 +1,55 @@
 <template>
-  <div class="single-card">
-    <img :src="categoria.image ? 'storage/' + categoria.image : ''" :alt="categoria.name">
+  <div class="single-card" @click="active()">
+    <img
+      :class="isActive ? 'active' : ''"
+      :src="categoria.image ? 'storage/' + categoria.image : ''"
+      :alt="categoria.name"
+    />
+    <h5>{{ categoria.name }}</h5>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Card",
+  data() {
+    return {
+      isActive: false,
+    };
+  },
   props: {
     categoria: Object,
-    
-  }
-}
+  },
+  methods: {
+    active: function () {
+      this.isActive ? (this.isActive = false) : (this.isActive = true);
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .single-card{
-    img {
-      width: 100%;
-      height: 120px;
-      object-fit: cover;
-    }
+@import "../../../../sass/_variables.scss";
+.single-card {
+  img {
+    width: 100%;
+    height: 120px;
+    object-fit: cover;
+    cursor: pointer;
+    margin-top: 10px;
+    border-radius: 12px;
+    transition: 0.5s all;
   }
-
+  img:hover {
+    filter: brightness(0.5);
+  }
+  h5 {
+    text-align: center;
+    margin-top: 10px;
+    color: rgba(0, 0, 0, 0.7);
+  }
+  .active {
+    filter: brightness(0.5);
+  }
+}
 </style>
