ab473124984895c062eabceeb4f3010377e728e1
AlessandroMandaglio30 <ale.manda.30@gmail.com>
2022-03-19 14:33:34 +0100

    fix-carello

--- resources/js/guest/components/elements/CartIcon.vue
+++ resources/js/guest/components/elements/CartIcon.vue
@@ -1,80 +1,96 @@
 <template>
-    <div>
-        <div @click="cartPopup()" class="container-cartIcon">
-            <i class="fa-solid fa-cart-shopping" ></i>
-            <div class="container-popup" :class="cartActive ? 'active' : ''">
-                <router-link class="goCart-btn" :to="{name: 'carrello', params: {title: 'carrello'}} ">
-                    <div>Vai al carrello</div>
-                    
-                </router-link>
-            </div>
-        </div>
-
-    </div>
+	<div>
+		<div @click="cartPopup()" class="container-cartIcon">
+			<i class="fa-solid fa-cart-shopping"></i>
+			<div class="container-popup" :class="cartActive ? 'active' : ''">
+				<div>
+					<ul>
+						<li
+							v-for="(element, index) in dataShared.cart"
+							:key="index"
+						>
+							{{ element.name }}
+						</li>
+					</ul>
+				</div>
+				<router-link
+					class="goCart-btn"
+					:to="{ name: 'carrello', params: { title: 'carrello' } }"
+				>
+					<div>Vai al carrello</div>
+				</router-link>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import dataShared from "../../dataShared";
 export default {
-    name: 'CartIcon',
-    data() {
-        return {
-            cartActive: false
-        }
-    },
-    methods: {
-        cartPopup: function () {
-            this.cartActive ? this.cartActive = false : this.cartActive = true;  
-        }
-    }
-}
+	name: "CartIcon",
+	data() {
+		return {
+			cartActive: false,
+			dataShared,
+		};
+	},
+	methods: {
+		cartPopup: function () {
+			this.cartActive
+				? (this.cartActive = false)
+				: (this.cartActive = true);
+		},
+	},
+};
 </script>
 
 <style scoped lang="scss">
 @import "../../../../sass/_variables.scss";
-    .container-cartIcon{
-        position: relative;
-    .container-popup{
-        background: white;
-        position: absolute;
-        width: 300px;
-        height: 300px;
-        z-index: 99;
-        top: 110%;
-        right: 0;
-        display: none;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-        border-radius: 15px;
-        &.active {
-            display: block;
-        }
-        a.goCart-btn{
-            text-decoration: none;
-            color: black;
-            >div {
-                display: inline-block;
-                background: $mainColor;
-                border: none;
-                border-radius: 30px;
-                padding: 10px;
-                font-size: 16px;
-                font-weight: 600;
-                
-            }
-        }
-    }
-    i{
-        display: block;
-        
-        padding: 17px;
-        font-size: 20px;
-        background-color: white;
-        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-        border-radius: 50%;
-        margin-right: 20px;
-        transition: 0.3s all;
-    }
-    i:hover{
-        color: $mainColor;
-    }
-    }
+.container-cartIcon {
+	position: relative;
+	.container-popup {
+		background: white;
+		position: absolute;
+		width: 400px;
+		height: 500px;
+		z-index: 99;
+		top: 110%;
+		right: 0;
+		display: none;
+		box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+			rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+		border-radius: 15px;
+		&.active {
+			display: block;
+		}
+		a.goCart-btn {
+			text-decoration: none;
+			color: black;
+			> div {
+				display: inline-block;
+				background: $mainColor;
+				border: none;
+				border-radius: 30px;
+				padding: 10px;
+				font-size: 16px;
+				font-weight: 600;
+			}
+		}
+	}
+	i {
+		display: block;
+
+		padding: 17px;
+		font-size: 20px;
+		background-color: white;
+		box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+			rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+		border-radius: 50%;
+		margin-right: 20px;
+		transition: 0.3s all;
+	}
+	i:hover {
+		color: $mainColor;
+	}
+}
 </style>
