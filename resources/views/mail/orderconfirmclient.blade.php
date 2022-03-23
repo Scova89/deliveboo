e49c25300836fa17b59a6d24f040f6399f6f080a
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2022-03-23 14:34:50 +0100

    fix model order

--- resources/views/mail/orderconfirmclient.blade.php
+++ resources/views/mail/orderconfirmclient.blade.php
@@ -1,3 +1,23 @@
 <h1>
     Ordine Confermato!
 </h1>
+<p>
+    Ciao <strong>{{$order->name}}</strong>, ti confermiamo che l'ordine effettuato è andato a buon fine!
+</p>
+
+<h2>
+    Riepilogo ordine:
+</h2>
+<ul>
+@foreach ($order->products as $product)
+    
+    <li>
+        <span>
+            {{ $product->name }}
+        </span>
+        <span>
+            x{{ $product->pivot->quantity }}
+        </span>
+    </li>
+@endforeach
+</ul>
