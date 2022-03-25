907b6baa0da997a9d5b9301cd3dc2e1749e03545
Filippo Rossi <rossifilippo2000@gmail.com>
2022-03-25 10:23:39 +0100

    header footer backend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body>
     <div id="app">
         <header>
-            <div class="box">
+            <div class="box container-fluid">
                 <div class="left">
                     <a href="{{url('/')}}">
                         <div class="logo">
@@ -78,6 +78,55 @@
                 @yield('content')
             </div>
         </main>
+        <footer>
+            <div class="footer-container">
+                <div class="col-12 col-xl-2 link">
+                    <div class="name">
+                        Filippo Rossi
+                    </div>
+                    <div class="box-link">
+                        <a href="https://github.com/FilippoR00" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/filippo-rossi" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-2 link">
+                    <div class="name">
+                        Ferdinando Letizia
+                    </div>
+                    <div class="box-link">
+                        <a href="https://github.com/ferdlet" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/ferdinando-letizia" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-2 link">
+                    <div class="name">
+                        Francesco Scovacricchi
+                    </div>
+                    <div class="box-link">
+                        <a href="https://github.com/Scova89" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        <a href="#" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-2 link">
+                    <div class="name">
+                        Alessandro Mandaglio
+                    </div>
+                    <div class="box-link">
+                        <a href="https://github.com/AlessandroMandaglio30" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/alessandro-mandaglio-9b4935232/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-12 col-xl-2 link">
+                    <div class="name">
+                        Alessandro Luisi
+                    </div>
+                    <div class="box-link">
+                        <a href="#" target="_blank"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://www.linkedin.com/in/alessandro-luisi-39b44a172" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </body>
 </html>
